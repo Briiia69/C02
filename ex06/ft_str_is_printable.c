db39c2d7b6064f6136aeb46589ed0feee48e0d03
Saliinger <alexisnoukan@gmail.com>
2026-06-23 12:58:13 +0200

    start

---
+++ ex06/ft_str_is_printable.c
@@ -0,0 +1,11 @@
+int ft_str_is_printable(char *str) {
+  int i;
+
+  i = 0;
+  while (str[i]) {
+    if (str[i] < 33 || str[i] == 127)
+      return (0);
+    i++;
+  }
+  return (1);
+}
---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,10 @@
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size) {
+  unsigned int i;
+
+  i = 0;
+  while (src[i] && i < size) {
+    dest[i] = src[i];
+    i++;
+  }
+  return (i);
+}
