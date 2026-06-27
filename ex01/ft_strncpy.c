70592b848955dfdaefb02533031d373b7cf85592
Saliinger <alexisnoukan@gmail.com>
2026-06-27 23:33:05 +0200

    almost

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -12,12 +12,17 @@
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (src[i] && i < n)
 	{
 		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -14,12 +14,16 @@ int	ft_str_is_alpha(char *str)
 {
 	int	i;
 
+	if (!str)
+		return (1);
 	i = 0;
 	while (str[i])
 	{
-		if (!(str[i] >= 97 && str[i] <= 122) || !(str[i] >= 65 && str[i] <= 90))
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A'
+				&& str[i] <= 'Z'))
+			i++;
+		else
 			return (0);
-		i++;
 	}
 	return (1);
 }
